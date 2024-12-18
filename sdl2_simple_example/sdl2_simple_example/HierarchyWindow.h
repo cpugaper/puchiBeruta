81eb9af04752cff868cb1199ae35e72e8ada1ccd
Carla Puga <84739899+cpugaper@users.noreply.github.com>
2024-12-18 11:45:45 +0100

    fixes

--- sdl2_simple_example/sdl2_simple_example/HierarchyWindow.h
+++ sdl2_simple_example/sdl2_simple_example/HierarchyWindow.h
@@ -6,7 +6,7 @@
 
 class HierarchyWindow {
 public:
-    void deleteSelectedObjects(std::vector<GameObject*>& gameObjects, std::vector<GameObject*>& selectedObjects);
+    void deleteSelectedObjects(std::vector<GameObject*>& gameObjects, std::vector<GameObject*>& selectedObjects, GameObject*& selectedObject);
     void deleteObjectAndChildren(GameObject* obj, std::vector<GameObject*>& gameObjects);
     void render(std::vector<GameObject*>& gameObjects, std::vector<GameObject*>& selectedObjects, GameObject*& selectedObject);
     void handleParenting(std::vector<GameObject*>& selectedObjects);
